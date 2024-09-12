a89d15942f903f489ccbf7fef09e7fc68cd6b525
Tanner Morgan <tmorgan497@gmail.com>
2024-09-12 15:16:45 -0500

    feat: Add new Tailwind CSS styled div
    Added a new div with Tailwind CSS classes for styling.

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,11 @@
     <h1>Index Page</h1>
     <Button @click="showDialog = true">Button</Button>
 
+    <div class="mt-4 bg-surface-500 p-4 rounded" v-tooltip="'mt-4 bg-surface-500 p-4 rounded'">
+        <span class="text-primary-200 border p-1 rounded" v-tooltip="'text-primary-200 border p-1 rounded'">Tailwind
+            Div</span>
+    </div>
+
     <Dialog v-model:visible="showDialog" modal>
         <template #header>
             <h3>Header</h3>
